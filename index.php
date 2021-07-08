961a848627f2371705ade2d7c852e91770e28867
CtrlAltDelit <netrogaidengi@gmail.com>
2021-07-08 08:42:02 +0400

    create first  block

--- index.php
+++ index.php
@@ -5,9 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>PHP</title>
 </head>
 <body>
-    абвгд
+    <?php
+        for($i = 0; $i < 5; $i++):
+    ?>
+        <div class="card shadow-sm container">
+            <svg class="bd-placeholder-img card-img-top" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Добавить</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Удалить</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Редакторовать</button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                </div>
+            </div>
+        </div>
+      <?php endfor; ?>
 </body>
 </html>
