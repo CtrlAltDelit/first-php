5f4fabc023c9174145008787274f548bdaa25931
CtrlAltDelit <netrogaidengi@gmail.com>
2021-07-08 09:45:07 +0400

    update 1

--- index.php
+++ index.php
@@ -13,16 +13,15 @@
         for($i = 0; $i < 5; $i++):
     ?>
         <div class="card shadow-sm container">
-            <svg class="bd-placeholder-img card-img-top" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="img-bank" src="img/<?php echo ($i + 1) ?>.jpg" alt="photo-bank">
             <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text">Российский финансовый конгломерат, крупнейший транснациональный и универсальный банк России, Центральной и Восточной Европы.</p>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                         <button type="button" class="btn btn-sm btn-outline-secondary">Добавить</button>
                         <button type="button" class="btn btn-sm btn-outline-secondary">Удалить</button>
                         <button type="button" class="btn btn-sm btn-outline-secondary">Редакторовать</button>
                     </div>
-                    <small class="text-muted">9 mins</small>
                 </div>
             </div>
         </div>
